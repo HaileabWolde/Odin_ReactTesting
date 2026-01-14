9c3fe7e5f47e24b9120a55f54936e04cffc95ab5
Haileab <womahaileab1428@gmail.com>
2026-01-14 15:59:35 +0300

    finshing the logic

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,13 @@
 import './cartComponent.css'
-import CardComponent from '../SingleItem/cardComponent'
+import CardComponent from '../SingleItem/CardComponent'
 const CartComponent = ({cartItems, deleteFromCart})=> {
   
-  
+    const allSum =  Math.round(cartItems.reduce((accumlator, currentValue)=>{
+                    const sum = currentValue.quantity * currentValue.price;
+                    accumlator = accumlator + sum;
+                    return accumlator
+                }, 0))
+              
     return (
         <div className="cart">
             <h1 className='text-[hsl(14,86%,42%)] font-extrabold text-2xl'>
@@ -27,6 +32,19 @@ const CartComponent = ({cartItems, deleteFromCart})=> {
                 })
             }
                 </ul>
+          {
+                cartItems.length > 0 && 
+              <div className='flex justify-around items-center'>
+                <p className='text-[hsl(7,20%,60%)] font-bold'>Order Total</p>
+                  <h1 className="text-[hsl(14,65%,9%)] font-black text-2xl">${allSum}</h1>
+              </div>
+          }
+          {
+            cartItems.length > 0 && 
+            <button className='cursor-pointer w-3/4 py-3 px-3 rounded-3xl shadow-2xl bg-[hsl(14,86%,42%)] self-center text-[hsl(20,50%,98%)]'>
+                <p className='font-bold'>Confirm Order</p>
+          </button>
+          }
           
         </div>
     )
